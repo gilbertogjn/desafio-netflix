e9f7ee2c09ad32ae0bf2c902be6705587a49f9ed
Gilberto <gilbertogjn@gmail.com>
2022-05-04 16:41:13 -0400

    commit do projeto desafio netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:12,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        },
+        1700: {
+            items:8
+        }
+    }
+})
